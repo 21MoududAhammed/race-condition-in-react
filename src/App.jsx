3cf65e4d49f34b98fff22071e4a4212bd23bb12d
21MoududAhammed <21moudud@gmail.com>
2024-02-08 08:50:09 +0600

    race condition issue has solved

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,24 @@ import { generateMilliseconds, searchData } from "./utils";
 export default function App() {
   const [buttonNum, setButtonNum] = useState(null);
   const [value, setValue] = useState("");
-  const [ output, setOutput] = useState('')
+  const [output, setOutput] = useState("");
 
+  let ignore = false;
 
   useEffect(() => {
     const milliseconds = generateMilliseconds();
     searchData(buttonNum, milliseconds, value)
       .then((res) => {
-        setOutput(res);
+        if(!ignore){
+          setOutput(res);
+        }
       })
       .catch((err) => console.log(err));
+
+      //clean up the useEffect
+      return ()=>{
+        ignore = true;
+      }
   }, [value, buttonNum]);
   return (
     <div className="flex flex-col justify-center my-10">
